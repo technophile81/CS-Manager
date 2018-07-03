84b140805fb6a4e25e240fb7dd4e2d91cfe1d626
Technophile <patricia.snodgrass@gmail.com>
2018-07-03 12:03:44 -0500

    route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 import { Router, Route, Redirect } from "react-router-dom";
@@ -10,24 +9,21 @@ import MaterialList from './pages/MaterialList';
 class App extends Component {
   render() {
     return (
-     <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">This is my silly app</h1>
           </header>
           <p className="App-intro">
             Making sure this is working. Much testing! Why are you not routing?
         </p>
 
-          <Route exact path="/materials" render={
+          {/* <Route exact path="/materials" render={
             (props) => {
               return(<MaterialList {...props} />)
             }
-          } />
+          } /> */}
 
         </div>
-       </Router> 
     );
   }
 }
