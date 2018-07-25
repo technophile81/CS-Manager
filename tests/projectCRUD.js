4f787d4a8384accf9b1c1de2dca56d859314361b
Patricia <technophile.1981@gmail.com>
2018-07-25 13:03:38 -0500

    add read/update tests to project CRUD

--- tests/projectCRUD.js
+++ tests/projectCRUD.js
@@ -58,12 +58,62 @@ function runDeleteSuccessTest(resolve, reject) {
     });
 }
 
+
+function runReadSuccessTest(resolve, reject) {
+    axios.get('/api/projects').then((res) => {
+        resolve("OK");
+    }).catch((err) => {
+        resolve("Failed to get list of projects with status " + err.response.status);
+    });
+}
+
+
+function runUpdateFailTest(resolve, reject) {
+    axios.put('/api/projects/1234', {
+        name: 'Update Fail',
+    }).then((res) => {
+        resolve("Unexpectedly succeeded on bad update");
+    }).catch((err) => {
+        if (err.response.status === 404) {
+            resolve("OK");
+        } else {
+            resolve("Unexpected status " + err.response.status);
+        }
+    });
+}
+
+function runUpdateSuccessTest(resolve, reject) {
+    axios.post('/api/projects', {
+        name: 'Update Test',
+    }).then((res) => {
+        axios.put('/api/projects/' + res.data._id, {
+            name: 'Update Success',
+        }).then((res) => {
+            if (res.status === 200) {
+                resolve("OK");
+            } else {
+                resolve("Unexpected status " + res.status);
+            }
+        }).catch((err) => {
+            resolve("Failed to update project with status " + err.response.status);
+        });
+    }).catch((err) => {
+        resolve("Failed to create project with status " + err.response.status);
+    });
+}
+
+
 async function runTests() {
     console.log("runCreateFailTest: " + await new Promise(runCreateFailTest));
     console.log("runCreateSuccessTest: " + await new Promise(runCreateSuccessTest));
 
     console.log("runDeleteFailTest: " + await new Promise(runDeleteFailTest));
     console.log("runDeleteSuccessTest: " + await new Promise(runDeleteSuccessTest));
+
+    console.log("runReadSuccessTest: " + await new Promise(runReadSuccessTest));
+
+    console.log("runUpdateFailTest: " + await new Promise(runUpdateFailTest));
+    console.log("runUpdateSuccessTest: " + await new Promise(runUpdateSuccessTest));
 }
 
 
