9f65a70d99ab06ab062eddbea41e9e69ede816ee
Technophile <patricia.snodgrass@gmail.com>
2018-07-26 10:22:11 -0500

    add components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,48 +1,18 @@
-import React from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+import React from "react"
+import { Link } from "react-router-dom";
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Craftage</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#">Login</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+class NavBar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar">
+                <Link to="/profile">Profile</Link>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/shopping">Shopping</Link>
+            </nav>
+        )
+    }
 }
+
+export default NavBar;
